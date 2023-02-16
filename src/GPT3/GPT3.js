4fa37c5eec47bcfe9b3b6ab9aa6c7b16fdaefc00
Migush <Migushthe2nd@users.noreply.github.com>
2023-02-16 22:07:58 +0100

    Disable disturbing check

--- src/GPT3/GPT3.js
+++ src/GPT3/GPT3.js
@@ -41,12 +41,12 @@ class GPT3 {
                         const text = response.data.choices[0].text;
                         const textNoSpecialChars = GPT3._wordsOnly(text);
 
-                        const isDisturbing = await GPT3._classifyIsDisturbing(textNoSpecialChars);
+//                         const isDisturbing = await GPT3._classifyIsDisturbing(textNoSpecialChars);
 
                         this.prompt += text;
-                        let clean = personality.cleanOutput(text, isDisturbing);
+                        let clean = personality.cleanOutput(text, false);
                         if (isVoiceChat) clean = textNoSpecialChars;
-                        if (isDisturbing) clean = (isVoiceChat ? GPT3.DISTURBING_WARNING : GPT3.PRETTY_DISTURBING_WARNING) + clean;
+//                         if (isDisturbing) clean = (isVoiceChat ? GPT3.DISTURBING_WARNING : GPT3.PRETTY_DISTURBING_WARNING) + clean;
                         if (clean.trim().length > 0) {
                             resolve(clean);
 
